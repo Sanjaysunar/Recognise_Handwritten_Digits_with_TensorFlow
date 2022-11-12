380482a461d5d26c1e4cd92d712253faff2a546a
Sanjay Sunar <sansunar11@gmail.com>
2022-11-12 17:45:52 +0530

    Splitting dataset into train, validation and test

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import tensorflow as tf
 # Import MNIST dataset and store the image data in the variable 'mnist'
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/",one_hot=True) # ylabels are oh-encoded
+
+# split dataset
+n_train = mnist.train.num_examples  # 55,000
+n_validation = mnist.validation.num_examples  # 5000
+n_test = mnist.test.num_examples  # 10,000
+
+
