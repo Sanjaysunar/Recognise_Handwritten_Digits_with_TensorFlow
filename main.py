b5a5d2a3a8daa1ddffc58bded0053e69694090d9
Sanjay Sunar <sansunar11@gmail.com>
2022-11-12 17:51:43 +0530

    Defining the Neural Network Architecture and Parameter

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ n_validation = mnist.validation.num_examples  # 5000
 n_test = mnist.test.num_examples  # 10,000
 
 
+# Defining the Neural Network Architecture
+n_input = 784  # input layer (28x28 pixels)
+n_hidden1 = 512  # 1st hidden layer
+n_hidden2 = 256  # 2nd hidden layer
+n_hidden3 = 128  # 3rd hidden layer
+n_output = 10  # output layer (0-9 digits)
+
+learning_rate = 1e-4
+n_iterations = 1000
+batch_size = 128
+dropout = 0.5
+
+
