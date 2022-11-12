b0e2209bfe2971cd7e7ebeae3aad9f89f925ff45
Sanjay Sunar <sansunar11@gmail.com>
2022-11-12 18:17:59 +0530

    Building the TensorFlow Graph
    - Random Values from a truncated normal distribution
         - For the weights
         - For the biases
    - Setup the layers of the network
    - Using Adam Optimizer

--- main.py
+++ main.py
@@ -22,4 +22,39 @@ n_iterations = 1000
 batch_size = 128
 dropout = 0.5
 
+# Building the TensorFlow Graph
+X = tf.placeholder("float", [None, n_input])
+Y = tf.placeholder("float", [None, n_output])
+keep_prob = tf.placeholder(tf.float32)
 
+#Random Values from a truncated normal distribution
+# For the weights
+weights = {
+    'w1' : tf.Variable(tf.truncated_normal([n_input, n_hidden1], stddev=0.1)),
+    'w2' : tf.Variable(tf.truncated_normal([n_hidden1, n_hidden2], stddev=0.1)),
+    'w3' : tf.Variable(tf.truncated_normal([n_hidden2, n_hidden3], stddev=0.1)),
+    'out' : tf.Variable(tf.truncated_normal([n_hidden3, n_output], stddev=0.1))
+}
+
+# For the biases
+biases = {
+    'b1' : tf.Variable(tf.constant(0.1, shape=[n_hidden1])),
+    'b2' : tf.Variable(tf.constant(0.1, shape=[n_hidden2])),
+    'b3' : tf.Variable(tf.constant(0.1, shape=[n_hidden3])),
+    'out' : tf.Variable(tf.constant(0.1, shape=[n_output]))
+}
+
+# Setup the layers of the network
+layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
+layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
+layer_3 = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
+layer_drop = tf.nn.dropout(layer_3, keep_prob)
+output_layer = tf.matmul(layer_3, weights['out']) + biases['out']
+
+# Using Adam Optimizer
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(
+        labels=Y, logits=output_layer
+    ))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
