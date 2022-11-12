8b25bf71ead6880237bd8ccdc3f02c80f50e6f6a
Sanjay Sunar <sansunar11@gmail.com>
2022-11-12 17:40:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-# Import MNIST dataset and store in 'mnist'
+# Import MNIST dataset and store the image data in the variable 'mnist'
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True) # ylabels are oh-encoded
